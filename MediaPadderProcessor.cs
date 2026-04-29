da6acabdb532d749061f1649dbb11c064d844df6
Peter Egunjobi <masterpdon@gmail.com>
2026-04-29 12:47:49 +0100

    Rewritten to use scale filters if there will be no padding in the output. For Nvidia GPUs, CPU scale+pad will be used if there's padding and the video is not 8-bit.

--- MediaPadderProcessor.cs
+++ MediaPadderProcessor.cs
@@ -15,26 +15,47 @@ namespace MediaPadderPage
     {
         public async Task PadMedia(string inputPath, Rect inputRect, Size outputSize, string colour, bool isImage)
         {
+            var isOnlyScaling = inputRect is { X: 0, Y: 0 } && Math.Abs(inputRect.Width - outputSize.Width) < 1 && Math.Abs(inputRect.Height - outputSize.Height) < 1;
             progressPrimary.Report(0);
             centerTextPrimary.Report("0.0 %");
-            rightTextPrimary.Report("Padding...");
+            rightTextPrimary.Report(isOnlyScaling ? "Scaling..." : "Padding...");
 
-            var scalePadParams = $"scale={inputRect.Width}:{inputRect.Height},pad={outputSize.Width}:{outputSize.Height}:{inputRect.X}:{inputRect.Y}:{colour}";
+            var scalePadParams = $"scale={outputSize.Width}:{outputSize.Height}";
+            if(!isOnlyScaling) scalePadParams += $",pad={outputSize.Width}:{outputSize.Height}:{inputRect.X}:{inputRect.Y}:{colour}";
             if (isImage)
             {
                 await StartFfmpegProcess($"-i \"{inputPath}\" -vf \"{scalePadParams},setsar=1\" \"{GetOutputName(inputPath)}\"", ProgressHandler); // Images do not support hardware acceleration. (they do, but it is not worth the complexity)
             }
             else
             {
+                var gpuPixelFormat = await GetGpuPixelFormat(inputPath);
                 switch (gpuInfo?.Vendor)
                 {
                     case GpuVendor.Nvidia:
-                        scalePadParams = $"scale_cuda={inputRect.Width}:{inputRect.Height},pad_cuda={outputSize.Width}:{outputSize.Height}:{inputRect.X}:{inputRect.Y}:{colour}";
+                        if(isOnlyScaling) scalePadParams = $"scale_cuda={outputSize.Width}:{outputSize.Height}";
+                        else if (gpuPixelFormat == "nv12") //pad_cuda only works on 8-bit videos
+                        {
+                            scalePadParams = $"scale_cuda={inputRect.Width}:{inputRect.Height},pad_cuda={outputSize.Width}:{outputSize.Height}:{inputRect.X}:{inputRect.Y}:{colour}";
+                        }
+                        else //use plain CPU pad filter
+                        {
+                            var (hwDownArgs, hwUpArgs) = GpuInfo.FilterParams(gpuInfo, gpuPixelFormat);
+                            scalePadParams = $"{hwDownArgs}{scalePadParams}{hwUpArgs}";
+                        }
                         break;
-                    case GpuVendor.Amd or GpuVendor.Intel:
-                        var gpuPixelFormat = await GetGpuPixelFormat(inputPath);
-                        var (hwDownArgs, hwUpArgs) = GpuInfo.FilterParams(gpuInfo, gpuPixelFormat);
-                        scalePadParams = $"{hwDownArgs}{scalePadParams}{hwUpArgs}";
+                    case GpuVendor.Amd:
+                        {
+                            var (hwDownArgs, hwUpArgs) = GpuInfo.FilterParams(gpuInfo, gpuPixelFormat);
+                            scalePadParams = $"{hwDownArgs}{scalePadParams}{hwUpArgs}";
+                        }
+                        break;
+                    case GpuVendor.Intel:
+                        if(isOnlyScaling) scalePadParams = $"scale_qsv={outputSize.Width}:{outputSize.Height}";
+                        else
+                        {
+                            var (hwDownArgs, hwUpArgs) = GpuInfo.FilterParams(gpuInfo, gpuPixelFormat);
+                            scalePadParams = $"{hwDownArgs}{scalePadParams}{hwUpArgs}";
+                        }
                         break;
                 }
                 await StartFfmpegTranscodingProcessDefaultQuality([inputPath], GetOutputName(inputPath), $"-vf \"{scalePadParams},setsar=1\"", ProgressHandler);
